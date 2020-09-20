694aa4d038258aec32d3da26e4286ac6bc5f76e6
Pineapple Season <emsthai21@gmail.com>
2020-09-20 12:30:01 -0700

    added comments to highlight errors in code

--- BrokenCode/logic.js
+++ BrokenCode/logic.js
@@ -3,6 +3,7 @@ $("#factButton").on("click", function() {
 	$("#factText").text(booFacts[number])
 })
 
+// End quotes were incorrectly placed
 var booFacts = ["Boo is a pomeranian", "Boo's best friend is another pomeranian named Buddy", "Boo the Pomeranian was born on March 16, making him a Pisces", "Boo's favourite food is grass, Boo has released two books"]
 
 $("#textPink").on("click", function() {
@@ -17,7 +18,7 @@ $("#textGreen").on("click", function() {
 	$("#funText").css("color", "green")
 })
 
-
+// click was missing quotation marks
 $("#boxGrow").on("click", function() {
 	$("#box").animate({height:"+=35px", width:"+=35px"}, "fast");
 })
